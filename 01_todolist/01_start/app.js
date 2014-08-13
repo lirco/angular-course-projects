30c494e16e6c0f2844880079c882003b938c2342
lirco <lirco77@gmail.com>
2014-08-13 14:26:32 +0300

    HW1-04
    small bug fix - user can't add empty task

--- 01_todolist/01_start/app.js
+++ 01_todolist/01_start/app.js
@@ -1,18 +1,23 @@
-/**
- * if I remove the hard-coded tasks array, I will get a 10 lines code, excluding the '}' closers.
- */
-
 ( function () {
   function taskManager() {
-    this.tasks = [
-      { "title": "learn angular!", "description": "start from scratch, build your skills" },
-      { "title": "play with controllers!", "description": "make sure to understand whats going on"},
-      { "title": "try bootstrap", "description": "prototype a gui is easy"}
-    ];
+      this.tasks = [
+          {
+              "title": "learn angular!",
+              "description": "start from scratch, build your skills"
+          },
+          {
+              "title": "play with controllers!",
+              "description": "make sure to understand whats going on"
+          },
+          {
+              "title": "try bootstrap",
+              "description": "prototype a gui is easy"
+          }
+      ];
     this.addTask = function() {
       if (this.newTask) {
         this.tasks.push(this.newTask)
-        this.newTask = {};
+        this.newTask = '';
       }
     }
   }
